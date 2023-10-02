80e0f873ac89f543b5ee89ad8958317bf8fdd70a
Johannes <danielssonjohannes@gmail.com>
2023-10-02 13:44:16 +0200

    Setting up folder for GetData from a json.

---
+++ smarriga-asiatiska-recept/src/components/GetData.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2> {{post.title}}</h2>
+
+</template>
+<script>
+export default{
+  data(){
+    return{
+      post: {}
+    }
+  },
+  methods:{
+    getPosts(){
+      fetch('https://jau22-recept-grupp4-xzvarhmra742.reky.se/recipes')
+          .then(response => response.json())
+          .then(data=>this.post=data)
+    }
+  }
+}
+</script>
