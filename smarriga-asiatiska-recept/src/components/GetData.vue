5a544f7350eb1de7d8c4a7e15c52b93ec8a44474
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-03 11:35:37 +0200

    Testing out with css

--- smarriga-asiatiska-recept/src/components/GetData.vue
+++ smarriga-asiatiska-recept/src/components/GetData.vue
@@ -1,22 +1,28 @@
 <template>
-  <h2 v-for="singlePost in post" :key="singlePost.id"> {{singlePost.title}}</h2>
+  <div id="recipeBox" v-for="singlePost in post" :key="singlePost.id">
+    <h2> {{ singlePost.title }}</h2>
+    <p> {{singlePost.description}}</p>
+    <p> {{singlePost.timeInMins}}</p>
+    <p>{{singlePost.ingredients.length}}</p>
+  </div>
+
 </template>
 <script>
-export default{
-  data(){
-    return{
+export default {
+  data() {
+    return {
       post: null
     };
   },
   async created() {
     await this.getPosts();
   },
-  methods:{
-    async getPosts(){
+  methods: {
+    async getPosts() {
       await fetch('https://jau22-recept-grupp4-xzvarhmra742.reky.se/recipes')
           .then(response => response.json())
-          .then(data=>{
-            this.post=data
+          .then(data => {
+            this.post = data
             console.log(data)
           })
     }
