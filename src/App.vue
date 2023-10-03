37ea44b5b20e97f4b275283924875b7fc32a7d29
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-03 14:02:43 +0200

    Has the pull req fixed

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <get-data></get-data>
+  </div>
+</template>
+
+<script>
+ import GetData from "@/components/GetData.vue";
+export default {
+  components: {GetData}
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import './assets/main.css'
+console.log("Running main")
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
