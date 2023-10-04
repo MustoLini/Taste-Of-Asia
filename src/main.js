8f040af882e7562c909adfa475d7a9eec849ba6c
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-04 15:27:59 +0200

    Adding in to js files router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-console.log("Running main")
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Import your router configuration
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); // Use Vue Router
+
+app.mount('#app');
