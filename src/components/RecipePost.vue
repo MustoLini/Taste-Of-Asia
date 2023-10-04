fdc32f168e3d0346dd0af18382371da670334064
Josef Lilja <josef.lilja.skola@gmail.com>
2023-10-04 22:21:41 +0200

    Some Fixes

--- src/components/RecipePost.vue
+++ src/components/RecipePost.vue
@@ -2,8 +2,9 @@
   <div class="recipe-post">
     <h2> {{ post.title }}</h2>
     <p> {{post.description}}</p>
-    <p> {{post.timeInMins}}</p>
-    <p>{{post.ingredients.length}}</p>
+    <img src="https://media.istockphoto.com/id/1147544807/sv/vektor/ingen-miniatyr-bild-vektor-grafik.jpg?s=612x612&w=is&k=20&c=878rwM2GRo5W4nV_Nc21FtF26ZlKre6JM0p7e7mSlN0=" alt="placeholder">
+    <p> {{"Time in minutes: " + post.timeInMins}}</p>
+    <p>{{"Amount of ingredients: "+  post.ingredients.length}}</p>
   </div>
 </template>
 
