8d0725c0570c23b47e6445d0edf5a5c60ba307f9
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-06 12:47:08 +0200

    Finishing upp recipe-detail to be able to work

--- src/components/RecipeDetail.vue
+++ src/components/RecipeDetail.vue
@@ -1,31 +1,48 @@
 <template>
   <div class="recipe-detail">
-    <h2>{{recipe.title}}</h2>
-    <p>{{recipe.description}}</p>
-    <img src="recipe.imageUrl" alt="Recipe Image" />
-    <p>{{"Time in minutes: " + recipe.timeInMins}}</p>
-    <p>{{"Amount of ingredients: " + recipe.ingredients.length}}</p>
+    <h2>{{ recipe.title }}</h2>
+    <p>{{ recipe.description }}</p>
+    <img src="recipe.imageUrl" alt="Recipe Image"/>
+    <p>{{ "Time in minutes: " + recipe.timeInMins }}</p>
+    <p>{{ "Amount of ingredients: " + recipe.ingredients.length}}</p>
   </div>
 </template>
 
 <script>
-export default
-{
-  data(){
-    return{
-      recipe:null
-    };
-  },created() {
-    const recipeId = this.$route.params.id;
-    // Example: Fetch the recipe data from an API
-    // Replace this with your actual API endpoint
-    fetch(`/api/recipes/${recipeId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.recipe = data;
-        });
-  },
-}
+import {defineComponent} from 'vue'
+
+export default defineComponent(
+    {
+      data() {
+        return {
+          recipe: {}
+        };
+      }, async created() {
+        const recipeId = this.$route.params.id;
+        console.log(recipeId);
+        this.getRecipe(recipeId)
+
+      }, methods: {
+        async  getRecipe(id) {
+          await fetch(`https://jau22-recept-grupp4-xzvarhmra742.reky.se/recipes/${id}`)
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error('Does not work')
+                } else {
+                  return response.json();
+                }
+              })
+              .then((data) => {
+                this.recipe = data;
+              }).catch((error) => {
+            console.error('Error fetching recipe data: ', error)
+          });
+        }
+      }
+
+    }
+)
+
 </script>
 
 <style scoped>
