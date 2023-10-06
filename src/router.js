757cf630efe8eed161ce241dfecacd5de8257fc1
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-06 12:48:04 +0200

    update to router.js

--- src/router.js
+++ src/router.js
@@ -4,20 +4,20 @@ import RecipePost from '@/components/RecipePost.vue';
 import GetData from '@/components/GetData.vue';
 import RecipeDetail from "@/components/RecipeDetail.vue";
 
-
 const routes = [
     {
         path: '/',
+        name: 'get-data',
         component: GetData,
     },
     {
-        path: '/recipe/:id',
+        path: '/recipe/:id', // This route matches the button click in RecipePost.vue
         name: 'recipe-post',
         component: RecipePost,
         props: true,
     },
     {
-        path: '/recipe-detail/:id',
+        path: '/recipe-detail/:id', // This route should match the URL update
         name: "recipe-detail",
         component: RecipeDetail,
         props: true
