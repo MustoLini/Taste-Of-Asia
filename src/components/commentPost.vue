b88344a5941759b9a093f2b77bdeddefa1d2a77a
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-10-17 11:03:53 +0200

    Adding validation into commentPost and Thank you message when u have sent comment

--- src/components/commentPost.vue
+++ src/components/commentPost.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <input type="text" v-model="author">
+    <input type="text" v-model="author" placeholder="Author">
     <textarea v-model="commentText" placeholder="Write your comment"></textarea>
-    <button @click="addComment">Add Comment</button>
+    <button @click="addComment" :disabled="isSubmitDisabled">Add Comment</button>
+    <p v-if="showThankYouMessage" class="thank-you">Thank you for your comment!</p>
   </div>
 
   <div v-for="comment in comments" :key="comment.createdAt">
@@ -21,22 +22,33 @@ export default {
       commentText: "",
       createdAt: "",
       comments: [],
+      validationError: "",
+      showThankYouMessage: false,
     }
   },
   props: ['recipeId'],
 
   created() {
     this.getComments();
+  },computed:{
+    isSubmitDisabled(){
+      return !this.author.trim()|| !this.commentText.trim()
+    }
+
   },
 
   methods: {
     async addComment() {
+      if (!this.author||!this.commentText){
+        this.validationError="Both author and comment text are required";
+        return;
+      }
+      this.validationError = "";
    
       const comment = {
         comment: this.commentText,
         name: this.author,
       };
-      console.log("Adding Comments", comment)
       try {
         await fetch(`https://jau22-recept-grupp4-xzvarhmra742.reky.se/recipes/${this.recipeId}/comments`, {
           method: 'POST',
@@ -46,6 +58,8 @@ export default {
           body: JSON.stringify(comment)
         })
         this.$emit("add-comment", comment);
+        this.showThankYouMessage = true;
+        this.getComments();
 
         this.author = "";
         this.commentText = "";
